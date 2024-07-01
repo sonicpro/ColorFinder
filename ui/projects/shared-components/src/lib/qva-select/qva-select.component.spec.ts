0c833baf45f8476bd112a923586da57e3ff695fb
sonicpro <chegnenado@yahoo.com>
2024-07-01 17:40:47 +0300

    Made unit tests in "shared-components" library succeed.

--- ui/projects/shared-components/src/lib/qva-select/qva-select.component.spec.ts
+++ ui/projects/shared-components/src/lib/qva-select/qva-select.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { QvaSelectComponent } from './qva-select.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('QvaSelectComponent', () => {
   let component: QvaSelectComponent;
@@ -8,7 +11,8 @@ describe('QvaSelectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ QvaSelectComponent ]
+      declarations: [ QvaSelectComponent ],
+      imports: [ MatFormFieldModule, MatSelectModule, ReactiveFormsModule, BrowserAnimationsModule ],
     })
     .compileComponents();
 
